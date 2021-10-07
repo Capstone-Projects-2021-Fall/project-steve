201e11eddb83593e33598a1455700f092a121c48
Hunter <hjn6100@gmail.com>
2021-10-07 15:38:15 -0400

    Created template for the Speaker Control

---
+++ RPi/SpeakerControl.py
@@ -0,0 +1,38 @@
+class SpeakerControl:
+    volume = 0
+
+    # def __init__(self):
+
+    # Parameters:
+    #     - volume is an int indicating the desired volume level for speakers
+    #
+    # Description:
+    #     - Set the current volume of the RC car as specified
+    #
+    # Return:
+    #     - Returns nothing
+    def set_volume(self, volume):
+        pass
+
+    # Parameters:
+    #     - None
+    #
+    # Description:
+    #     - Return current volume level
+    #
+    # Return:
+    #     - Returns volume as an int
+    def get_volume(self):
+        pass
+
+    # Parameters:
+    #     - message is the string which dictates the message the speakers will play
+    #
+    # Description:
+    #     - Take in the message as a String and convert it to an audio file. Then speak the statement out loud
+    #
+    # Return:
+    #     - Returns nothing
+    def play_message(self, message):
+        pass
+
