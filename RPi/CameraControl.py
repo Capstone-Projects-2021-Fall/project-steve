8540070a3e8c71069f47429d1f729b0e8520875b
Hunter <hjn6100@gmail.com>
2021-10-07 16:10:48 -0400

    Created template for the Camera Control

---
+++ RPi/CameraControl.py
@@ -0,0 +1,12 @@
+class CameraControl:
+
+    # Parameters:
+    #     - None
+    #
+    # Description:
+    #     - Takes a real time image using the onboard camera
+    #
+    # Return:
+    #     - an image
+    def get_image(self):
+        pass
