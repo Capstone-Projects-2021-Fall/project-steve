d3dddde48dbe4129e0dc64cf3b40ce23041ce1cb
Blake Patterson <brpatterson00@gmail.com>
2021-10-15 20:35:39 -0400

    changed host url of the RPi server

--- RPi/server.py
+++ RPi/server.py
@@ -53,5 +53,5 @@ def receive_car_instructions():
 if __name__ == '__main__':
 
     # launch the app on localhost
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
     carControl = CarControl()
