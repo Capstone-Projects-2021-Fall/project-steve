5d791e207e72de1b885bff51c833464175fc02e7
Blake Patterson <brpatterson00@gmail.com>
2021-10-15 20:09:20 -0400

    added buttons to post data to remote server

--- MobileApp/steve/src/app/tab1/tab1.page.ts
+++ MobileApp/steve/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,53 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  speed = 0
+  turnVal = 90
+
   constructor() {}
+
+  forward() {
+    console.log('forward')
+    this.speed = .12;
+    this.postToRemoteServer(this.speed, this.turnVal);
+  }
+
+  left() {
+    console.log('left')
+    this.turnVal = 0;
+    this.postToRemoteServer(this.speed, this.turnVal);
+  }
+
+  center() {
+    console.log('center')
+    this.turnVal = 90;
+    this.postToRemoteServer(this.speed, this.turnVal);
+  }
+
+  right() {
+    console.log('right')
+    this.turnVal = 180;
+    this.postToRemoteServer(this.speed, this.turnVal);
+  }
+
+  stop() {
+    console.log('stop')
+    this.speed = 0;
+    this.postToRemoteServer(this.speed, this.turnVal);
+  }
+
+  postToRemoteServer(speed, turnVal) {
+    $.ajax({
+      type: "POST",
+      url: "http://127.0.0.1:9999/controlCar",
+      data: {
+        "speed": speed,
+        "turnVal": turnVal
+      },
+      success: function(data) {
+        console.log('successfully posted')
+      }
+    });
+  }
 
 }
