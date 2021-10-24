9df3d25be02672b35881cef706f6300cdf377946
Blake Patterson <brpatterson00@gmail.com>
2021-10-24 19:34:00 -0400

    added start_xbox_manual_control code

--- RPi/client.py
+++ RPi/client.py
@@ -1,4 +1,5 @@
 import requests
+import pygame
 
 class Client:
     target_host = ''
@@ -27,3 +28,47 @@ class Client:
         print(url)
         response = requests.post(url, json=request_body)
         print(response)
+
+
+    def start_xbox_manual_control(self):
+        pygame.init()
+
+        pygame.joystick.init()
+        clock = pygame.time.Clock()
+
+        print
+        pygame.joystick.get_count()
+        _joystick = pygame.joystick.Joystick(0)
+        _joystick.init()
+
+        while 1:
+            for event in pygame.event.get():
+                if event.type == pygame.JOYBUTTONDOWN:
+                    print("Joystick button pressed.")
+                    print(event)
+                if event.type == pygame.JOYAXISMOTION:
+                    # print _joystick.get_axis(0)
+                    # print event
+                    if event.axis == 0:  # this is the x axis
+                        print(event.value)
+                    if event.axis == 5:  # right trigger
+                        print(event.value)
+            xdir = _joystick.get_axis(0)
+
+            rtrigger = _joystick.get_axis(5)
+            # deadzone
+            if abs(xdir) < 0.2:
+                xdir = 0.0
+            if rtrigger < -0.9:
+                rtrigger = -1.0
+
+            print([xdir, rtrigger])
+
+            clock.tick(30)
+
+        pygame.quit()
+
+
+if __name__ == '__main__':
+    client = Client("dummy", 0000)
+    client.start_xbox_manual_control()
