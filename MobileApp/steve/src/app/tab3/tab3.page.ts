b64458a2e2ea6fd3e0b1e3afe06ad4fd4e6bee61
Blake Patterson <brpatterson00@gmail.com>
2021-10-29 21:26:17 -0400

    moved & updated config values, and read routes from Firebase

--- MobileApp/steve/src/app/tab3/tab3.page.ts
+++ MobileApp/steve/src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, onValue} from "firebase/database";
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +9,29 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor() {
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyCiBPkwDTFuiOrRtKc3ZKUAh_xwvSb8WSI",
+      authDomain: "steve-2efa6.firebaseapp.com",
+      databaseURL: "https://steve-2efa6-default-rtdb.firebaseio.com",
+      projectId: "steve-2efa6",
+      storageBucket: "steve-2efa6.appspot.com",
+      messagingSenderId: "650370236834",
+      appId: "1:650370236834:web:0bca880b2938e86c04f2fb",
+      measurementId: "G-LJ8TNR05TL"
+    };
+  
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+
+    const db = getDatabase();
+    const routeRef = ref(db, 'route');
+    onValue(routeRef, (snapshot) => {
+      const data = snapshot.val();
+      console.log(data)
+    });
+
+  }
 
 }
