d16cd47862a90a8edf99ee4fdbe3ba2e9b3f890a
Blake Patterson <brpatterson00@gmail.com>
2021-12-03 18:11:07 -0500

    wrote BehavioralCloningHelper tests

--- RemoteServer/test_BehavioralCloningHelper.py
+++ RemoteServer/test_BehavioralCloningHelper.py
@@ -1,9 +1,15 @@
 from unittest import TestCase
+from BehavioralCloningHelper import BehavioralCloningHelper
+
+behavioralCloningHelper = BehavioralCloningHelper()
 
 
 class TestBehavioralCloningHelper(TestCase):
+
     def test_train_model(self):
-        self.fail()
+        result = behavioralCloningHelper.train_model(.12, 90, "steves_eyes.jpg")
+        self.assert_(result)
 
     def test_get_instruction(self):
-        self.fail()
+        result = behavioralCloningHelper.get_instruction(.12, 90, "steves_eyes.jpg")
+        self.assert_(result)
