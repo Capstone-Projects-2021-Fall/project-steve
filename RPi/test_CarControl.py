c2ed8ea392fb13d09e366f8edf8926fe913883eb
Blake Patterson <brpatterson00@gmail.com>
2021-12-03 18:23:24 -0500

    wrote CarControl tests

--- RPi/test_CarControl.py
+++ RPi/test_CarControl.py
@@ -1,21 +1,50 @@
 from unittest import TestCase
+from CarControl import CarControl
+
+carControl = CarControl()
 
 
 class TestCarControl(TestCase):
     def test_set_speed(self):
-        self.fail()
+        speed = .15
+        carControl.set_speed(speed)
+        self.assertEqual(carControl.get_speed(), speed)
 
     def test_get_speed(self):
-        self.fail()
+        speed = carControl.get_speed()
+        flag = False
+        if 0 <= int(speed) <= .15:
+            flag = True
+        self.assert_(flag)
 
     def test_gradual_speed_up(self):
-        self.fail()
+        init_speed = .10
+        speed = .15
+        carControl.set_speed(init_speed)
+        carControl.gradual_speed_up(speed, .5)
+        flag = False
+        if init_speed <= carControl.get_speed() <= speed:
+            flag = True
+        self.assert_(flag)
 
     def test_set_turn_val(self):
-        self.fail()
+        turn_val = 180
+        carControl.set_turn_val(turn_val)
+        self.assertEqual(carControl.get_turn_val(), turn_val)
 
     def test_get_turn_val(self):
-        self.fail()
+        turn_val = carControl.get_turn_val()
+        flag = False
+        if 0 <= int(turn_val) <= 180:
+            flag = True
+        self.assert_(flag)
 
     def test_gradual_turn(self):
-        self.fail()
+        init_turn_val = 0
+        turn_val = 180
+        carControl.set_turn_val(init_turn_val)
+        carControl.gradual_turn(turn_val, .5)
+        flag = False
+        if init_turn_val <= carControl.get_turn_val() <= turn_val:
+            flag = True
+        self.assert_(flag)
