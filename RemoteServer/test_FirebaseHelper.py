3027f4a1fd40dadcb9cff54adbfffe8fa11cdafa
Hunter <hjn6100@gmail.com>
2021-12-03 18:23:06 -0500

    Created register location unit test

--- RemoteServer/test_FirebaseHelper.py
+++ RemoteServer/test_FirebaseHelper.py
@@ -1,11 +1,39 @@
 from unittest import TestCase
 from FirebaseHelper import FirebaseHelper
+import pyrebase
+
+firebaseConfig = {
+    "apiKey": "AIzaSyCiBPkwDTFuiOrRtKc3ZKUAh_xwvSb8WSI",
+    "authDomain": "steve-2efa6.firebaseapp.com",
+    "databaseURL": "https://steve-2efa6-default-rtdb.firebaseio.com",
+    "projectId": "steve-2efa6",
+    "storageBucket": "steve-2efa6.appspot.com",
+    "messagingSenderId": "650370236834",
+    "appId": "1:650370236834:web:0bca880b2938e86c04f2fb",
+    "measurementId": "G-LJ8TNR05TL"
+    }
+
+firebase = pyrebase.initialize_app(firebaseConfig)
 
 firebaseHelper = FirebaseHelper()
 
 class TestFirebaseHelper(TestCase):
     def test_register_location(self):
-        self.fail()
+        location = "TESTREGISTERLOCATION"
+        firebaseHelper.register_location(location)
+        flag = False
+
+        db = firebase.database()
+        locations = db.child("location").get()
+
+        s = ""
+        for key, value in locations.val().items():
+            s = s + value["location"] + ", "
+            if value["location"] == location:
+                flag = True
+
+        print(s)
+        self.assertTrue(flag)
 
     def test_register_route(self):
         route = "TESTROUTE"
